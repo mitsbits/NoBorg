6daf7aa8c64230dadf8c3274b7d277b441f3bc08
Dimitris Bitsanis <mitsbits@users.noreply.github.com>
2017-12-11 11:31:44 +0200

    Update StringExtensions.cs

--- src/infra/Borg.Infra/ExtensionMethods/StringExtensions.cs
+++ src/infra/Borg.Infra/ExtensionMethods/StringExtensions.cs
@@ -20,5 +20,29 @@ namespace Borg.Infra.ExtensionMethods
 
             return true;
         }
+                public static string RemoveWhitespace(this string str)
+        {
+            return string.Join("", str.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        public static string PadToStartAndEndOnceOnly(this string str, char padChar)
+        {
+            var padString = new string(new[] { padChar });
+
+            var copy = string.Copy(str);
+
+            while (copy.StartsWith(padString) || copy.EndsWith(padString))
+                copy = copy.Trim(padChar);
+
+            return $"\"{copy}\"";
+        }
+
+        public static string MakeValidFileName(this string name)
+        {
+            string invalidChars = System.Text.RegularExpressions.Regex.Escape(new string(System.IO.Path.GetInvalidFileNameChars()));
+            string invalidRegStr = $@"([{invalidChars}]*\.+$)|([{invalidChars}]+)";
+
+            return System.Text.RegularExpressions.Regex.Replace(name, invalidRegStr, "_");
+        }
     }
 }
