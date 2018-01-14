68973f63582dca78e8c4ea2830836e27dd6db3a7
mitsbits <mitsbits@gmail.com>
2018-01-14 11:27:46 +0200

    namespace fix

--- src/infra/Borg.Infra/Storage/Assets/AssetCreatedEventHandler.cs
+++ src/infra/Borg.Infra/Storage/Assets/AssetCreatedEventHandler.cs
@@ -1,5 +1,7 @@
-﻿using Borg.Infra.Storage.Assets;
-using System;
+﻿using System;
 using System.Threading.Tasks;
 
-public delegate Task AssetCreatedEventHandler<TKey>(AssetCreatedEventArgs<TKey> args) where TKey : IEquatable<TKey>;
+namespace Borg.Infra.Storage.Assets
+{
+    public delegate Task AssetCreatedEventHandler<TKey>(AssetCreatedEventArgs<TKey> args) where TKey : IEquatable<TKey>;
+}
