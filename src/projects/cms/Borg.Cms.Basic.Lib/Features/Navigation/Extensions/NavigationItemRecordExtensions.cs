82fa5acd55324d39a508eb5139b1ad91483c7516
mitsbits <mitsbits@gmail.com>
2018-01-22 19:06:50 +0200

    forgotten files

---
+++ src/projects/cms/Borg.Cms.Basic.Lib/Features/Navigation/Extensions/NavigationItemRecordExtensions.cs
@@ -0,0 +1,83 @@
+﻿using Borg.Infra.DTO;
+using System.Collections.Generic;
+using System.Linq;
+using Borg.Cms.Basic.Lib.Features.Navigation;
+
+namespace Borg
+{
+    internal static class NavigationItemRecordExtensions
+    {
+        public static Tidings Trees(this IEnumerable<NavigationItemRecord> source)
+        {
+            var roots = source.Where(x => x.ParentId == 0 || !source.Any(r => x.ParentId == r.Id));
+            var result = new Tidings();
+            foreach (var root in roots)
+            {
+                result.Add(ToTiding(root, source));
+            }
+            return result;
+        }
+
+        public static Tidings Trees(this IEnumerable<NavigationItemRecord> source, int rootId)
+        {
+            var roots = source.Where(x => x.Id == rootId);
+            var result = new Tidings();
+            foreach (var root in roots.OrderBy(x=>x.Weight).ThenBy(x=>x.Display))
+            {
+                result.Add(ToTiding(root, source));
+            }
+            return result;
+        }
+
+        public static IDictionary<(int, int), Tiding> TreeDictionaryExcludingCurrentAndChildren(this IEnumerable<NavigationItemRecord> source, Tiding current)
+        {
+            var navigationItemRecords = source as NavigationItemRecord[] ?? source.ToArray();
+            var entity = navigationItemRecords.FirstOrDefault(x => x.Id.ToString() == current?.Key);
+            return navigationItemRecords.TreeDictionaryExcludingCurrentAndChildren(entity);
+        }
+
+        public static IDictionary<(int, int), Tiding> TreeDictionaryExcludingCurrentAndChildren(this IEnumerable<NavigationItemRecord> source, NavigationItemRecord current)
+        {
+            var navigationItemRecords = source.ToList();
+            var tree = navigationItemRecords.Trees().TreeDictionary();
+            var hit = tree.FirstOrDefault(x => x.Key.Item1 == current.Id);
+            if (hit.Key.Item1 == 0 && hit.Key.Item2 == 0) return tree;
+
+            var keystoexclude = new List<(int, int)>();
+            RecurseToExclude(ref keystoexclude, current.Id, tree);
+            foreach (var exclude in keystoexclude)
+            {
+                tree.Remove(exclude);
+            }
+            return tree;
+        }
+
+        private static void RecurseToExclude(ref List<(int, int)> keystoexclude, int id, IDictionary<(int, int), Tiding> source)
+        {
+            var current = source.FirstOrDefault(x => x.Key.Item1 == id);
+            if (current.Key.Item1 == 0 && current.Key.Item2 == 0) return;
+            keystoexclude.Add(current.Key);
+            foreach (var child in current.Value.Children)
+            {
+                RecurseToExclude(ref keystoexclude, int.Parse(child.Key), source);
+            }
+        }
+
+        private static Tiding ToTiding(NavigationItemRecord record, IEnumerable<NavigationItemRecord> source, int level = 1)
+        {
+            var tiding = new Tiding(record.Id.ToString(), record.Display)
+            {
+                Weight = record.Weight,
+                Flag = record.IsPublished.ToString(),
+                Hint = record.ItemType.ToString(),
+                HumanKey = record.Path,
+                Depth = level
+            };
+            foreach (var row in source.Where(x => x.ParentId == record.Id).OrderBy(x => x.Weight).ThenBy(x => x.Display))
+            {
+                tiding.Children.Add(ToTiding(row, source, level + 1));
+            }
+            return tiding;
+        }
+    }
+}
---
+++ src/projects/cms/Borg.Cms.Basic.Lib/Features/Navigation/Extensions/TidingExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Borg.Infra.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+
+namespace Borg
+{
+    public static class TidingExtensions
+    {
+        public static string ToBootstrapTree(this Tidings tidings, IUrlHelper url, string @group, int? row)
+        {
+            var bucket = new List<dynamic>();
+            foreach (var root in tidings.AsEnumerable().OrderBy(x => x.Weight).ThenBy(x => x.Value))
+            {
+                bucket.Add(PopulateNode(root, url, @group, row));
+            }
+            var toserialize = bucket.ToArray();
+            return JsonConvert.SerializeObject(toserialize);
+        }
+
+        private static dynamic PopulateNode(Tiding root, IUrlHelper url, string @group, int? row, bool parentEnabled = true)
+        {
+            dynamic node = new ExpandoObject();
+            node.text = root.Value;
+            node.href = url.Action("Home", "Menus", new { id = @group, row = root.Key });
+            node.selectable = false;
+            var isEnabled = bool.Parse(root.Flag);
+            dynamic state = new { @checked = isEnabled, selected = row.HasValue && row.Value.ToString() == root.Key };
+            var tags = new[] { root.Hint, root.Weight.ToString("n2") };
+            node.tags = tags;
+            node.state = state;
+            if (!parentEnabled || !isEnabled) node.color = "#a94442";
+
+            var children = new List<dynamic>();
+            foreach (var child in root.Children.AsEnumerable().OrderBy(x => x.Weight).ThenBy(x => x.Value))
+            {
+                children.Add(PopulateNode(child, url, @group, row, isEnabled));
+            }
+            if (children.Any()) node.nodes = children.ToArray();
+            return node;
+        }
+    }
+}
