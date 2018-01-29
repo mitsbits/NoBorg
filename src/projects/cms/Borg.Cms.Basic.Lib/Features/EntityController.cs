735e04a30eca5f33f89b8b460745f51a8911d9b7
mitsbits <mitsbits@gmail.com>
2018-01-29 20:10:39 +0200

    forgotten files

---
+++ src/projects/cms/Borg.Cms.Basic.Lib/Features/EntityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Borg.Infra.DDD.Contracts;
+using Borg.MVC;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Borg.Cms.Basic.Lib.Features
+{
+    public abstract class EntityController<TEntity, TKey, TDbContext> : BorgController where TEntity : IEntity<TKey> where TKey : IEquatable<TKey> where TDbContext :DbContext
+    {
+        protected EntityController(ILoggerFactory loggerFactory) : base(loggerFactory)
+        {
+        }
+    }
+}
