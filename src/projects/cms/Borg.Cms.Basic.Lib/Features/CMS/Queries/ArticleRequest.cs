c7468efafb5b16cc919260a399fd8a05366addba
mitsbits <mitsbits@gmail.com>
2018-02-01 19:12:13 +0200

    forgotten files

---
+++ src/projects/cms/Borg.Cms.Basic.Lib/Features/CMS/Queries/ArticleRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Borg.Infra.DAL;
+using Borg.Platform.EF.CMS;
+using Borg.Platform.EF.CMS.Data;
+using Borg.Platform.EF.Contracts;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Borg.Cms.Basic.Lib.Features.CMS.Queries
+{
+    public class ArticleRequest : IRequest<QueryResult<ArticleState>>
+    {
+        public ArticleRequest(int recordId)
+        {
+            RecordId = recordId;
+        }
+
+        public int RecordId { get; }
+    }
+
+    public class ArticleRequestHandler : AsyncRequestHandler<ArticleRequest, QueryResult<ArticleState>>
+    {
+        private readonly ILogger _logger;
+        private readonly IUnitOfWork<CmsDbContext> _uow;
+
+        public ArticleRequestHandler(ILoggerFactory loggerFactory, IUnitOfWork<CmsDbContext> uow)
+        {
+            _logger = loggerFactory.CreateLogger(GetType());
+            _uow = uow;
+        }
+
+        protected override async Task<QueryResult<ArticleState>> HandleCore(ArticleRequest message)
+        {
+            try
+            {
+                var result = await _uow.QueryRepo<ArticleState>().Get(x => x.Id == message.RecordId, CancellationToken.None, a => a.Tags, a => a.Component);
+
+                return QueryResult<ArticleState>.Success(result);
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e);
+                return QueryResult<ArticleState>.Failure(e.Message);
+            }
+        }
+    }
+}
---
+++ src/projects/cms/Borg.Cms.Basic.Lib/Features/Device/Extensions/DeviceStateExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Borg.CMS.BuildingBlocks;
+using Borg.MVC.BuildingBlocks;
+using Borg.Platform.EF.CMS;
+
+namespace Borg
+{
+    internal static class DeviceStateExtensions
+    {
+        public static DeviceStructureInfo DeviceStructureInfo(this DeviceState hit)
+        {
+            var result = new DeviceStructureInfo()
+            {
+                RenderScheme = hit.RenderScheme,
+                Layout = hit.Layout
+            };
+            foreach (var sectionRecord in hit.Sections)
+            {
+                var section = new Section()
+                {
+                    FriendlyName = sectionRecord.FriendlyName,
+                    Identifier = sectionRecord.Identifier,
+                    RenderScheme = sectionRecord.RenderScheme
+                };
+                foreach (var slotRecord in sectionRecord.Slots)
+                {
+                    try
+                    {
+                        var values = slotRecord.Module(sectionRecord.Identifier);
+                        section.DefineSlot(values.slotInfo, values.renderer);
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+                result.Sections.Add(section);
+            }
+            return result;
+        }
+    }
+}
