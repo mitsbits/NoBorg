8756f441435dbb1ad9f098004d7cc4a611378b10
mitsbits <mitsbits@gmail.com>
2018-02-11 21:02:41 +0200

    tags

---
+++ src/projects/cms/Borg.Cms.Basic.Lib/Features/CMS/Commands/ArticleTagsAssociationCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Borg.Infra.DAL;
+using Borg.Infra.Services.Slugs;
+using Borg.Platform.EF.CMS;
+using Borg.Platform.EF.CMS.Data;
+using Borg.Platform.EF.Contracts;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Borg.Cms.Basic.Lib.Features.CMS.Commands
+{
+   public class ArticleTagsAssociationCommand : CommandBase<CommandResult>
+    {
+        public int RecordId { get; }
+
+        public string[] Tags { get; }
+
+    }
+
+
+    public class ArticleTagsAssociationCommandHandler : AsyncRequestHandler<ArticleTagsAssociationCommand, CommandResult>
+    {
+        private readonly ILogger _logger;
+
+        private readonly IUnitOfWork<CmsDbContext> _uow;
+
+        private readonly IMediator _dispatcher;
+
+        private readonly ISlugifierService _slugifier;
+
+        public ArticleTagsAssociationCommandHandler(ILoggerFactory loggerFactory, IUnitOfWork<CmsDbContext> uow, IMediator dispatcher, ISlugifierService slugifier)
+        {
+            _uow = uow;
+            _dispatcher = dispatcher;
+            _slugifier = slugifier;
+            _logger = loggerFactory.CreateLogger(GetType());
+        }
+
+        protected override async Task<CommandResult> HandleCore(ArticleTagsAssociationCommand message)
+        {
+            try
+            {
+                var article = await _uow.ReadWriteRepo<ArticleState>().Get(x => x.Id == message.RecordId,
+                    CancellationToken.None, x => x.ArticleTags);
+                article.ArticleTags.Clear();
+                foreach (var messageTag in message.Tags)
+                {
+                    var tag = messageTag.Trim();
+                }
+                await _uow.Save();
+                return CommandResult.Success();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(1, ex, "Error creating/updating html snippet from {@message} - {exception}", message, ex.ToString());
+                return CommandResult<HtmlSnippetState>.FailureWithEmptyPayload(ex.ToString());
+            }
+        }
+    }
+}
---
+++ src/projects/cms/Borg.Cms.Basic.Lib/Features/CMS/Commands/RenameArticleCommand.cs
@@ -0,0 +1,84 @@
+﻿using Borg.Cms.Basic.Lib.Features.CMS.Events;
+using Borg.Infra.DAL;
+using Borg.Infra.Services.Slugs;
+using Borg.Platform.EF.CMS;
+using Borg.Platform.EF.CMS.Data;
+using Borg.Platform.EF.Contracts;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace Borg.Cms.Basic.Lib.Features.CMS.Commands
+{
+    public class RenameArticleCommand : CommandBase<CommandResult>
+    {
+        [Required]
+        public int RecordId { get; set; }
+
+        [Required]
+        [MaxLength(1024)]
+        [DisplayName("Title")]
+        public string NewTitle { get; set; }
+
+        [DisplayName("Set Slug")]
+        public bool AlsoSetSlug { get; set; } = false;
+    }
+
+    public class RenameArticleCommandHandler : AsyncRequestHandler<RenameArticleCommand, CommandResult>
+    {
+        private readonly ILogger _logger;
+
+        private readonly IUnitOfWork<CmsDbContext> _uow;
+
+        private readonly IMediator _dispatcher;
+
+        private readonly ISlugifierService _slugifier;
+
+        public RenameArticleCommandHandler(ILoggerFactory loggerFactory, IUnitOfWork<CmsDbContext> uow, IMediator dispatcher, ISlugifierService slugifier)
+        {
+            _uow = uow;
+            _dispatcher = dispatcher;
+            _slugifier = slugifier;
+            _logger = loggerFactory.CreateLogger(GetType());
+        }
+
+        protected override async Task<CommandResult> HandleCore(RenameArticleCommand message)
+        {
+            try
+            {
+                ArticleRenamedEvent @event = null;
+                var article = await _uow.ReadWriteRepo<ArticleState>().Get(x => x.Id == message.RecordId);
+                if (article == null)
+                {
+                    _logger.Warn("Article with id {id} was not found", message.RecordId);
+                    return CommandResult.Failure($"Article with id {message.RecordId} was not found");
+                }
+
+                var oldTitle = article.Title;
+                var newTitle = message.NewTitle;
+                var oldslug = article.Slug;
+                var newslug = article.Slug;
+                if (message.AlsoSetSlug)
+                {
+                    newslug = _slugifier.Slugify(newTitle, 1024);
+                }
+
+                article.Title = newTitle;
+                article.Slug = newslug;
+                await _uow.ReadWriteRepo<ArticleState>().Update(article);
+                await _uow.Save();
+                @event = new ArticleRenamedEvent(message.RecordId, oldTitle, newTitle, oldslug, newslug);
+                _dispatcher.Publish(@event);
+                return CommandResult.Success();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(1, ex, "Error renaming articlet from {@message} - {exception}", message, ex.ToString());
+                return CommandResult<HtmlSnippetState>.FailureWithEmptyPayload(ex.ToString());
+            }
+        }
+    }
+}
