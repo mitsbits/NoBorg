bf5692f179d879d1d1b3a0c03efa8e2a64d2d8ec
mitsbits <mitsbits@gmail.com>
2018-02-20 23:41:52 +0200

    ιγνορεδ βυ μιστακε

---
+++ src/projects/cms/Borg.Cms.Basic.Lib/Features/CMS/Commands/AppendDocumentToComponentCommand.cs
@@ -0,0 +1,15 @@
+﻿using Borg.Infra.DAL;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Borg.Cms.Basic.Lib.Features.CMS.Commands
+{
+    public class AppendDocumentToComponentCommand : CommandBase<CommandResult>
+    {
+        public int RecordId { get; set; }
+        public IFormFile File { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
---
+++ src/projects/cms/Borg.Cms.Basic.Lib/Features/CMS/Events/ComponentDocumentAssociationEvent.cs
@@ -0,0 +1,18 @@
+﻿using Borg.Infra.Messaging;
+using MediatR;
+
+namespace Borg.Cms.Basic.Lib.Features.CMS.Events
+{
+    internal class ComponentDocumentAssociationEvent : MessageBase, INotification
+    {
+        public ComponentDocumentAssociationEvent(int componentId, int documentId)
+        {
+            ComponentId = componentId;
+            DocumentId = documentId;
+        }
+
+        public int ComponentId { get; }
+        public int DocumentId { get; }
+
+    }
+}
