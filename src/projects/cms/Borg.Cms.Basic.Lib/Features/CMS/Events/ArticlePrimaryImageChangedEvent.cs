d0eae48b5a037f4ffb92e9144ab5476668df8549
Dimitris Bitsanis <mitsbits@gmail.com>
2018-02-20 11:25:22 +0200

    ignored by mistake

---
+++ src/projects/cms/Borg.Cms.Basic.Lib/Features/CMS/Events/ArticlePrimaryImageChangedEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Borg.Infra.Messaging;
+using MediatR;
+
+namespace Borg.Cms.Basic.Lib.Features.CMS.Events
+{
+
+    class ArticlePrimaryImageChangedEvent : MessageBase, INotification
+    {
+        public ArticlePrimaryImageChangedEvent(int recordId, (int documentId, int fileId) current, (int? documentId, int? fileId) prev)
+        {
+            RecordId = recordId;
+            Current = current;
+            Previous = prev;
+        }
+        public int RecordId { get; }
+        private (int documentId, int fileId) Current;
+        private (int? documentId, int? fileId) Previous;
+
+
+    }
+}
