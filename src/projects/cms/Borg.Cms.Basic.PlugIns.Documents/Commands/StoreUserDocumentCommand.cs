20b5a9a1384ddd7224e526d370de1baadad78bb5
mitsbits <mitsbits@gmail.com>
2018-02-20 21:19:44 +0200

    IDocumentsService bug fix

--- src/projects/cms/Borg.Cms.Basic.PlugIns.Documents/Commands/StoreUserDocumentCommand.cs
+++ src/projects/cms/Borg.Cms.Basic.PlugIns.Documents/Commands/StoreUserDocumentCommand.cs
@@ -30,11 +30,13 @@ namespace Borg.Cms.Basic.PlugIns.Documents.Commands
         private readonly ILogger _logger;
         private readonly IMediator _dispatcher;
         private readonly IAssetStore<AssetInfoDefinition<int>, int> _assetStore;
+        private readonly IAssetStoreDatabaseService<int> _assetStoreDatabase;
 
-        public StoreUserDocumentCommandHandler(ILoggerFactory loggerFactory, IMediator dispatcher, IAssetStore<AssetInfoDefinition<int>, int> assetStore)
+        public StoreUserDocumentCommandHandler(ILoggerFactory loggerFactory, IMediator dispatcher, IAssetStore<AssetInfoDefinition<int>, int> assetStore, IAssetStoreDatabaseService<int> assetStoreDatabase)
         {
             _dispatcher = dispatcher;
             _assetStore = assetStore;
+            _assetStoreDatabase = assetStoreDatabase;
             _logger = loggerFactory.CreateLogger(GetType());
             _assetStore.FileCreated += args => _dispatcher.Publish(new FileCreatedEvent(args.RecordId, args.MimeType));
         }
@@ -48,8 +50,8 @@ namespace Borg.Cms.Basic.PlugIns.Documents.Commands
                 if (definition == null) return CommandResult<(int docid, int fileid)>.FailureWithEmptyPayload($"Could not create document from {filename}");
                 var commandResult = await _dispatcher.Send(new DocumentInitialCommitCommand(definition.Id, message.UserHandle));
                 if (!commandResult.Succeded) return CommandResult<(int docid, int fileid)>.FailureWithEmptyPayload(commandResult.Errors);
-                var spec = await _assetStore.Spec(definition.Id);
-                return CommandResult<(int docid, int fileid)>.Success((docid: definition.Id, fileid: spec.Id));
+                var currentspec = await _assetStoreDatabase.CurrentFile(definition.Id);
+                return CommandResult<(int docid, int fileid)>.Success((docid: definition.Id, fileid: currentspec.Id));
             }
             catch (Exception ex)
             {
