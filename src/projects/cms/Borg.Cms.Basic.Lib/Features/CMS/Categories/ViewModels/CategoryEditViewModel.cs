2853435edf75492179a0229916cd7f88f0c6162a
mitsbits <mitsbits@gmail.com>
2018-03-04 12:03:26 +0200

    ignored by mistake

---
+++ src/projects/cms/Borg.Cms.Basic.Lib/Features/CMS/Categories/ViewModels/CategoryEditViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Borg.Infra.DTO;
+using System.Collections.Generic;
+
+namespace Borg.Cms.Basic.Lib.Features.CMS.Categories.ViewModels
+{
+    public class CategoryEditViewModel
+    {
+        public int RecordId { get; set; } = 0;
+        public string FriendlyName { get; set; }
+        public string Slug { get; set; }
+        public int GroupingId { get; set; } = 0;
+        public int ParentId { get; set; } = 0;
+
+        public bool IsTransient => RecordId == default(int);
+        public IDictionary<(int, int), Tiding> ParentOptions { get; set; }
+        public bool AlsoSetSlug { get; set; } = false;
+    }
+}
---
+++ src/projects/cms/Borg.Cms.Basic.Lib/Features/CMS/Categories/ViewModels/CategoryGroupingViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Borg.Platform.EF.CMS;
+using System.Linq;
+using Borg.Infra.DTO;
+
+namespace Borg.Cms.Basic.Lib.Features.CMS.Categories.ViewModels
+{
+    public class CategoryGroupingViewModel
+    {
+        public CategoryGroupingState AggregateRoot { get; set; }
+        public int SelectedCategoryId { get; set; } = default(int);
+
+        public CategoryEditViewModel CategoryEditViewModel()
+        {
+            if (HasCategorySelected)
+            {
+                var hit = AggregateRoot.Categories.Single(x => x.Id == SelectedCategoryId);
+                return new CategoryEditViewModel()
+                {
+                    ParentId = hit.Taxonomy.ParentId,
+                    FriendlyName = hit.FriendlyName,
+                    Slug = hit.Slug,
+                    RecordId = hit.Id,
+                    GroupingId = hit.GroupingId,
+                    ParentOptions = TreeDictionaryExcludingCurrentAndChildren()
+                };
+            }
+
+            return new CategoryEditViewModel()
+            {
+                ParentOptions =  TreeDictionary()
+            };
+        }
+
+        private bool HasCategorySelected => SelectedCategoryId > 0;
+
+        public Tidings Trees() => AggregateRoot.Categories.Trees();
+
+        public IDictionary<(int, int), Tiding> TreeDictionary() => Trees().TreeDictionary();
+
+        public IDictionary<(int, int), Tiding> TreeDictionaryExcludingCurrentAndChildren() =>
+            AggregateRoot.Categories.TreeDictionaryExcludingCurrentAndChildren(SelectedState);
+
+
+        public CategoryState SelectedState => AggregateRoot.Categories.FirstOrDefault(x => x.Id == SelectedCategoryId);
+    }
+}
